a99b277c9383a7e31b2fe60f0bd2f2fff2a8ac8d
Didi416 <didiprins@optusnet.com.au>
2024-10-04 11:39:31 +0000

    Update

--- rs1_project/src/scanComparisonMatch.cpp
+++ rs1_project/src/scanComparisonMatch.cpp
@@ -1,6 +1,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <nav_msgs/msg/occupancy_grid.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -8,15 +9,19 @@
 
 class Localiser : public rclcpp::Node {
 public:
-    Localiser() : Node("scan_to_image_node"){
-        scan_subscriber_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/scan", 10, std::bind(&Localiser::scanCallback, this, std::placeholders::_1));
+    Localiser() : Node("localiser_node"){
+        scan_subscriber_ = this->create_subscription<sensor_msgs::msg::LaserScan>("scan", 10, std::bind(&Localiser::scanCallback, this, std::placeholders::_1));
         cmd_publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-        map_subscriber_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>("/map", 10, std::bind(&Localiser::mapCallback, this, std::placeholders::_1));
+        map_subscriber_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>("map", 10, std::bind(&Localiser::mapCallback, this, std::placeholders::_1));
+        odo_subscriber_ = this->create_subscription<nav_msgs::msg::Odometry>("amcl_pose", 10, std::bind(&Localiser::odoCallback, this, std::placeholders::_1));
         RCLCPP_INFO(this->get_logger(), "Localiser Node started.");
         cv::namedWindow(WINDOW1, cv::WINDOW_AUTOSIZE);
     }
 
 private:
+    void odoCallback(const nav_msgs::msg::Odometry::SharedPtr msg){
+        odo_ = *msg;
+    }
     void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg){
         occupancyGridToImage(msg);
 
@@ -25,7 +30,7 @@ private:
         cv::rotate(mapImage_, mapImage_, cv::ROTATE_90_COUNTERCLOCKWISE);
 
         cv::imshow("Map Image", mapImage_);
-        cv::waitKey(30);
+        cv::waitKey(1);
         map_image_captured_ = true;
     }
 
@@ -36,7 +41,7 @@ private:
         // cv::imshow("Scan Image", scanImage_);
         // cv::waitKey(1);
         
-        if (map_image_captured_) {
+        if (map_image_captured_ && !localised_) {
             localiseTurtleBot(mapImage_, scanImage_);  
         }
     }
@@ -64,38 +69,19 @@ private:
                 angle_difference_ = angle_difference_ * 180.0 / CV_PI;
                 
                 RCLCPP_INFO(this->get_logger(), "Estimated yaw angle change: %f degrees", angle_difference_);
+
+
             }
         } catch (const cv::Exception& e) {
             RCLCPP_ERROR(this->get_logger(), "Error in estimateAffinePartial2D: %s", e.what());
         }
 
         if (std::abs(angle_difference_) < 1.0){
+            localised_ = true;
             std::cout << "Localised successfully!" << std::endl;
         }
         else{rotateRobot();}
 
-        // Estimate transformation between scan points and map points using findHomography
-        // cv::Mat homography = cv::findHomography(scanPoints, mapPoints, cv::RANSAC);
-        // if (homography.empty()) {
-        //     // If the homography cannot be computed, return the initial pose
-        //     std::cerr << "Homography computation failed!" << std::endl;
-        //     return 0;
-        // }
-        // Apply the transformation (homography) to estimate the robot's new pose
-        // std::vector<cv::Point2f> untransformedPoints(1, initialPose); // Use robot origin as reference
-        // std::vector<cv::Point2f> transformedPoints;
-        // cv::perspectiveTransform(untransformedPoints, transformedPoints, homography);
-        // Convert the transformed point to pixel coordinates
-        // cv::Point2f targetPose = transformedPoints[0] * mapResolution;
-        // if (untransformedPoints == transformedPoints){
-        //     std::cout << "Localised successfully!" << std::endl;
-        //     rotateRobot();
-        // }
-        // geometry_msgs::msg::Twist movementCmd = calculateMovement(initialPose, targetPose);
-        // // Publish the movement commands to the TurtleBot
-        // cmd_publisher_->publish(movementCmd);
-        // // Add a small delay to let the robot execute the movement
-        // rclcpp::sleep_for(std::chrono::seconds(2));
         return 0;
     }
 
@@ -133,7 +119,7 @@ private:
                 float angle = scan->angle_min + i * scan->angle_increment;
                 unsigned int x = static_cast<int>((range * cos(angle)) * imageSize_x / (2 * max_range)) + imageSize_x / 2;
                 unsigned int y = static_cast<int>((range * sin(angle)) * imageSize_y / (2 * max_range)) + imageSize_y / 2;
-                if (x >= 0 && x < imageSize_x && y >= 0 && y < imageSize_y) {
+                if (x < imageSize_x && y < imageSize_y) {
                     image.at<uchar>(y, x) = 255; //if within range, mark with a white dot on image
                 }
             }
@@ -210,18 +196,20 @@ private:
 
         // Display the image to verify
         cv::imshow("Occupancy Grid", m_MapColImage);
-        cv::waitKey(30);
+        cv::waitKey(1);
     }
 
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_subscriber_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_publisher_;
     rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_subscriber_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odo_subscriber_;
 
     cv::Mat mapImage_, scanImage_;
     bool map_image_captured_ = false;
-    bool second_image_captured_ = false;
+    bool localised_ = false;
 
     double angle_difference_;
+    nav_msgs::msg::Odometry odo_;
 
     nav_msgs::msg::OccupancyGrid grid_;
 
